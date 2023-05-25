9533e6b90a7ce19121def7fbfe92257f4ba10868
Lidhya <lidhyajeejan@gmail.com>
2023-05-25 23:26:28 +0530

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    spacing: {
+      fourty: "40px",
+      sixty: "60px",
+      eighty: "80px",
+      zero: "0px",
+    },
+    extend: {
+      colors: {
+        backgroundColor: "#F5F5F5",
+      },
+    },
   },
   plugins: [],
-}
-
+};
