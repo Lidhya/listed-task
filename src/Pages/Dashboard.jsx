080ad9d66c99459e46191e9c7e448db54e5845b2
Lidhya <lidhyajeejan@gmail.com>
2023-05-26 16:05:26 +0530

    Dashboard completed

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import TotalBlocks from '../Components/TotalBlocks';
+import LineGraph from '../Components/LineGraph'; 
+import BottomBlock1 from '../Components/BottomBlock1'; 
+import BottomBlock2 from '../Components/BottomBlock2';
 
 const Dashboard = () => {
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col h-full'>
           <Navbar/>
           <TotalBlocks/>
+          <LineGraph/>
+          <div className='flex flex-row gap-10 h-[30%] mt-3 flex-wrap sm:flex-nowrap font-montserrat'>
+            <BottomBlock1/>
+            <BottomBlock2/>
+          </div>
         </div>
     );
 }
--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -6,8 +6,8 @@ const Layout = () => {
   return (
     <div class="bg-backgroundColor border-none m-0 p-0 ">
         <div className="flex h-screen w-full">
-        <div className="hidden md:block md:w-[20%] m-9 mr-0"><LeftBar/></div>
-        <div className="md:w-[70%] m-14 mb-20 "><Dashboard/></div>
+        <div className="hidden md:block md:w-[20%] m-7 mr-0"><LeftBar/></div>
+        <div className="md:w-[70%] w-full sm:m-14 "><Dashboard/></div>
         </div>
     </div>
   );
